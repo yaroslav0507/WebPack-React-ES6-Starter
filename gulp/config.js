21093131375ad56ba07957c92f7b0dce0db1443e
Yaroslav Zaklinskyi <Yaroslav_Zaklinskyi@epam.com>
2015-12-23 14:37:17 +0200

    npm module - merge2 instead of sequence-stream

--- gulp/config.js
+++ gulp/config.js
@@ -4,25 +4,40 @@ module.exports = function () {
 
     var template = './client/index.html';
 
+    var views = ['./client/**/*.html'];
+
     var components = [
         './client/src/app/app.js',
         './client/src/**/*.js',
         '!./client/src/**/*.spec.js'
     ];
 
+    var styles = [
+        './client/scss/core.scss'
+    ];
+
     var vendorScripts = [
         './client/vendors/angular/angular.js',
-        './client/vendors/jquery/dist/jquery.js',
-        './client/vendors/bootstrap/dist/js/bootstrap.js'
+        './client/vendors/angular-aria/angular-aria.js',
+        './client/vendors/angular-animate/angular-animate.js',
+        './client/vendors/angular-material/angular-material.js',
+        './client/vendors/angular-messages/angular-messages.js'
+    ];
+
+    var vendorStyles = [
+        './client/vendors/angular-material/angular-material.min.css'
     ];
 
     return {
         paths: {
             src: {
                 template: template,
+                views: views,
                 components: components,
+                styles: styles,
                 vendors: {
-                    scripts: vendorScripts
+                    scripts: vendorScripts,
+                    styles: vendorStyles
                 }
             },
             development: './dist/',
--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var seq = require('sequence-stream');
+var merge = require('merge2');
 var concat = require('gulp-concat');
 var templateCache = require('gulp-angular-templatecache');
 var sourcemaps = require('gulp-sourcemaps');
@@ -11,11 +11,12 @@ gulp.task('scripts', function(){
 
     var app = gulp.src(config.paths.src.components, {base: 'src'});
 
-    var views = gulp.src(config.paths.src.template).pipe(templateCache({
-        module: 'app'
-    }));
+    var views = gulp.src(config.paths.src.views)
+        .pipe(templateCache({
+            module: 'app'
+        }));
 
-    return seq([vendors, app, views])
+    return merge([vendors, app], views)
         .pipe(sourcemaps.init())
         .pipe(concat('app.js'))
         .pipe(sourcemaps.write('maps', {sourceRoot: '/client'}))
