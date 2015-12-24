f273b411d39d4251949b3ac76cd72566c95f850a
Yaroslav Zaklinskyi <Yaroslav_Zaklinskyi@epam.com>
2015-12-24 18:08:36 +0200

    webpack - inject: body, homePage template

--- webpack/components/plugins.js
+++ webpack/components/plugins.js
@@ -3,7 +3,7 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = [
     new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
-        title: 'projectX app',
-        template: path.join(PATHS.app, '/index.html')
+        template: path.join(PATHS.app, '/index.html'),
+        inject: 'body'
     })
 ];
