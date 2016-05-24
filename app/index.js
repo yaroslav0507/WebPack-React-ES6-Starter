7d040f4432c527e5541e1b51b9353cb2a1297f33
Yaroslav Zaklinskyi <yaroslav0507@gmail.com>
2016-05-24 19:28:16 +0300

    communication between sibling components via store

--- app/index.js
+++ app/index.js
@@ -4,10 +4,20 @@ require("./scss/core.scss");
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
-import HomePage from './pages/home/homePage';
+import Layout from './pages/Layout';
+import Archives from './pages/Todos';
+import Favorites from './pages/Favorites';
+import Settings from './pages/Settings';
 
 ReactDOM.render(
-    <HomePage />,
-    document.getElementById('content')
+    <Router history={hashHistory}>
+	<Route path="/" component={Layout}>
+	    <IndexRoute component={Favorites}></IndexRoute>
+	    <Route path="todos(/:todo)" name="todos" component={Archives}></Route>
+	    <Route path="settings" name="settings" component={Settings}></Route>
+	</Route>
+    </Router>,
+    document.getElementById('app')
 );
